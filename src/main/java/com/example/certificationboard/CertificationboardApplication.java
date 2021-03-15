393ffad9cc742b030ed309547be4873f31b4914f
sangyongchoi <csy7792@naver.com>
2021-03-16 00:03:32 +0900

    first commit

---
+++ src/main/java/com/example/certificationboard/CertificationboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.certificationboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertificationboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/certificationboard/CertificationboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.certificationboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
