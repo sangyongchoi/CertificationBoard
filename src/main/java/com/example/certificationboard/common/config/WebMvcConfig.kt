70edc38ae90f2f24faac6268c1032bc5cb904e35
sangyongchoi <csy7792@naver.com>
2021-05-02 03:34:53 +0900

    안쓰는 import 제거

--- src/main/java/com/example/certificationboard/common/config/WebMvcConfig.kt
+++ src/main/java/com/example/certificationboard/common/config/WebMvcConfig.kt
@@ -6,7 +6,6 @@ import org.springframework.data.web.PageableHandlerMethodArgumentResolver
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @Configuration
